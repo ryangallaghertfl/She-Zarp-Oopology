c45e04e63c7b63f861799ce5422e3c7dc345f7ed
Ryan Gallagher <RyanGallagher@tfl.gov.uk>
2023-01-17 10:39:15 +0000

    added PK to models

--- Oopology/Models/User.cs
+++ Oopology/Models/User.cs
@@ -1,7 +1,10 @@
-﻿namespace Oopology.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oopology.Models
 {
     public class User
     {
+        [Key]
         public string Id { get; set; }
         public string Name { get; set; }
         public int XpLevel { get; set; }

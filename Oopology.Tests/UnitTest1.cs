fd2a530ebbcc889a45c464e2fc9e72d589421c46
Samuel Hunt <samuelhunt@tfl.gov.uk>
2023-01-17 14:59:31 +0000

    Tests folder set up

---
+++ Oopology.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Oopology.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(12, 12);
+        }
+    }
+}

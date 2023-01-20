e6768aa009ca2f6ce10db5a4a0eb76bb14e5409a
Muhammad Azeem <115223231+BIGMVP26@users.noreply.github.com>
2023-01-20 12:05:00 +0000

    Update HomeController.cs

--- Oopology/Controllers/HomeController.cs
+++ Oopology/Controllers/HomeController.cs
@@ -8,6 +8,50 @@ namespace Oopology.Controllers
         {
             return View();
         }
+        [Route("/signout")]
+        [HttpGet]
+
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+        [Route("/signout2")]
+        [HttpGet]
+
+        public IActionResult SignOut2()
+        {
+            return View();
+        }
+        [Route("/signout3")]
+        [HttpGet]
+
+        public IActionResult SignOut3()
+        {
+            return View();
+        }
+        [Route("/signout4")]
+        [HttpGet]
+
+        public IActionResult SignOut4()
+        {
+            return View();
+        }
+        [Route("/signout5")]
+        [HttpGet]
+
+        public IActionResult SignOut5()
+        {
+            return View();
+        }
+        [Route("/signoutfrfr")]
+        [HttpGet]
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync();
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
 
        
     }

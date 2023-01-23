a332fbfd3f1c050fc16464ee0eac4592eb1c1b6b
Ryan Gallagher <RyanGallagher@tfl.gov.uk>
2023-01-23 16:58:24 +0000

    Added purchase button in basket and added purchase form

---
+++ Oopology/Models/PurchaseDetail.cs
@@ -0,0 +1,17 @@
+﻿using static Oopology.Models.Purchase;
+using System.IO.Pipelines;
+
+namespace Oopology.Models
+{
+    public class PurchaseDetail
+    {
+        public int PurchaseDetailId { get; set; }
+        public int PurchaseId { get; set; }
+        public int CourseId { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public Course Course { get; set; } = default!;
+        public Purchase Purchase { get; set; } = default!;
+        
+    }
+}
